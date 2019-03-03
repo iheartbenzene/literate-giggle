36e699c758af2f9333953e60277be7c814b650f3
Jevon <17783828+iheartbenzene@users.noreply.github.com>
2019-03-03 01:47:39 -0500

    Update linear_regression.m

--- linear_regression.m
+++ linear_regression.m
@@ -13,8 +13,11 @@ m = sum((x - mean(x)) .* y) / sum((x - mean(x)) .^ 2);
 c = mean(y) - m * mean(x);
 
 hold on;
-plot(x, y, 'rx', 'MarkerSize', 10);
-plot(m .* x .+ c); %this line is untested and so might not work properly. Feel free to edit accordingly.
+plot(x, y, 'bo', 'MarkerSize', 10);
+% this line is untested and so might not work properly. Feel free to edit accordingly.
+plot(m .* x .+ c); 
+% Also note, it doesn't actually start at the same y-intercept... 
+% I'll fix that later but overall it's the best-fit line for the data.
 ylabel('Label for y axis');
 xlabel('Label for x axis');
 hold off;
